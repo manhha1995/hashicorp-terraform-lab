c8095cefaf7b4126cd10cf73e006b428cb56197e
Nguyen Manh Ha <manhhanguyen95@hotmail.com>
2024-02-21 14:02:32 +0700

    add security grouops

--- main.tf
+++ main.tf
@@ -63,6 +63,42 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+
+locals {
+  ingress_rules = [{
+    port = 443
+    description = "Port 443"
+  },
+  {
+    port = 80
+    description = "Port 80"
+  }
+  ]
+}
+
+resource "aws_security_group" "security_groups" {
+  vpc_id = aws_vpc.vpc.id
+
+  dynamic "ingress" {
+    for_each = local.ingress_rules
+
+    content {
+      description = ingress.value.description
+      from_port = ingress.value.port
+      to_port = ingress.value.port
+      protocol = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy = true
+  }
+}
+
 module "vpc" {
   source = "./modules/vpc"
   subnet_id = ""
